e7dae72d61dcec936e03231158d70e5fe3de1ba3
monikop <mmonikopp@gmail.com>
2023-11-29 01:58:47 +0300

    some changes

--- db/database.js
+++ db/database.js
@@ -1,13 +1,9 @@
 module.exports = {
   development: {
-    // username: process.env.DB_USER,
-    // password: process.env.DB_PASS,
-    // database: process.env.DB_NAME,
-    // host: process.env.DB_HOST,
-    username: "monikop",
-    password: "23022005",
-    database: "s2s",
-    host: "127.0.0.1",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "postgres",
     seederStorage: "sequelize",
     seederStorageTableName: "SequelizeData",
