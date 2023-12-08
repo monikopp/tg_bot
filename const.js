cbb3e34c139c09226e8d9ea1c5a390dd80337319
monikop <mmonikopp@gmail.com>
2023-12-08 21:55:39 +0300

    del

--- const.js
+++ const.js
@@ -2,15 +2,7 @@ const commands = [
   { command: "/start", description: "Запустить бота" },
   { command: "/menu", description: "Открыть меню" },
 ];
-const commandsForNew = [{ command: "/start", description: "Запустить бота" }];
-const like = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [
-      [{ text: "1.❤️", callback_data: "like" }],
-      [{ text: "2.👎", callback_data: "dislike" }],
-    ],
-  }),
-};
+
 const likeKeyboard = [["1.❤️", "2.👎", "3.Вернуться в меню"]];
 const menuKeyboard = [
   ["1.Смотреть свою анкету", "2.Изменить анкету"],
@@ -24,15 +16,6 @@ const editProfileKeyboard = [
   ["Назад"],
 ];
 
-// const langKeyboard = [
-//   ["Английский", "Китайский", "Корейский"],
-//   ["Японский", "1", "2"],
-//   ["3", "4", "5"],
-// ];
-
-// const sexKeyboard=[
-//   ["Мужской", "Женский"],
-// ]
 module.exports = {
   likeKeyboard,
   commands,
